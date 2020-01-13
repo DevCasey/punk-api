4ee643fc7f775a4b781628849eb2db4a7de6fbf4
Casey Calkins <c.angelcalkins@outlook.com>
2020-01-12 18:14:12 -0600

    Fetching from Beer api, working out how to display to the DOM

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLoading: true,
+      beers: []
+    }
+  }
+
+  componentDidMount() {
+    this.fetchBeer();
+  }
+
+  fetchBeer() {
+    fetch('https://api.punkapi.com/v2/beers')
+    .then(response => response.json())
+    .then(parsedJSON => console.log(parsedJSON.results))
+    .catch(error => console.log('parsing failed', error))
+  }
+
+  render() {
+    return (  
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <button onClick={this.fetchBeer}>Fetch Beer</button>
+        </header>
+      </div>
+    )
+  };
 }
 
 export default App;
