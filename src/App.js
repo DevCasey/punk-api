30f1a52fa7ac4a74461b324ce22f8ef3d7414333
Casey Calkins <c.angelcalkins@outlook.com>
2020-01-12 19:29:12 -0600

    Trying to display beers

--- src/App.js
+++ src/App.js
@@ -8,28 +8,34 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      isLoading: true,
       beers: []
     }
   }
 
   componentDidMount() {
-    this.fetchBeer();
+    fetch('https://api.punkapi.com/v2/beers')
+      .then((response) => { return response.json()} )
+      .then(json =>       { this.setState({beers: json})} )
+      .then(console.log(this.state.beers))
+      .catch(error => console.log('parsing failed', error))
   }
 
-  fetchBeer() {
-    fetch('https://api.punkapi.com/v2/beers')
-    .then(response => response.json())
-    .then(parsedJSON => console.log(parsedJSON.results))
-    .catch(error => console.log('parsing failed', error))
-  }
+
+
+  // fetchBeer() {
+  //   fetch('https://api.punkapi.com/v2/beers')
+  //     .then((response) => { return response.json() })
+  //     .then((response) => { console.log(response) })
+  //     .catch(error => console.log('parsing failed', error))
+  // }
 
   render() {
     return (  
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <button onClick={this.fetchBeer}>Fetch Beer</button>
+            <button onClick={this.displayBeer}>Fetch Beer</button> 
+          {/* <button onClick={this.}>Fetch Beer</button> */}
         </header>
       </div>
     )
